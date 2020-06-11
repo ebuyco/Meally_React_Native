e8ed8f621ee82d1158831cbd3b93755a6cbdafd6
ErnieBuyco <buycoernie@gmail.com>
2020-06-11 16:41:08 +0800

    design-system

---
+++ Meal_App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native'
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return(
+    <NavigationContainer>
+        
+    </NavigationContainer>
+  )
+}
