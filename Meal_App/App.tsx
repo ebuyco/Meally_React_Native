79f1160ed6271496275c4d6de5f4d6eaabf8bc1c
ErnieBuyco <buycoernie@gmail.com>
2020-06-12 14:21:48 +0800

    content

--- Meal_App/App.tsx
+++ Meal_App/App.tsx
@@ -7,7 +7,20 @@ const Stack = createStackNavigator();
 const App = () => {
   return(
     <NavigationContainer>
-        
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Home"
+                component={SearchScreen}
+                options={{ title: 'Food Search'}}
+                />
+                <Stack.Screen
+                name="FoodDetails"
+                component={FoodDetailScreen}
+                />
+
+        </Stack.Navigator>
     </NavigationContainer>
   )
 }
+
+export default App;
